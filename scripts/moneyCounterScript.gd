a8ddef23b82c87304390c989a0a262bdadf32587
kazooing <amiridanish26@gmail.com>
2025-11-23 12:23:04 +0700

    feat: added some UIs and tidies the money counter so that it won't go off the bar

--- scripts/moneyCounterScript.gd
+++ scripts/moneyCounterScript.gd
@@ -1,10 +1,21 @@
 extends Label
 
+
 func _onready() -> void:
 	if GM.restart == 1:
 		GM.curMoney = 0
 
 func addMoney (amount):
+	GM.money_gained_per_scene += 1000
 	GM.curMoney += amount
 	GM.money_gained_per_scene += amount
-	text = ": $" + str(GM.money_gained_per_scene)
+	fit_to_text_size()
+	text = "$" + str(GM.money_gained_per_scene)
+
+func fit_to_text_size():
+	var current_money = GM.money_gained_per_scene
+	if(current_money<100): add_theme_font_size_override("font_size", 30)
+	elif(current_money >= 100 && current_money < 1000): add_theme_font_size_override("font_size", 29)
+	elif(current_money > 1000 && current_money < 10000): add_theme_font_size_override("font_size", 24) 
+	else: add_theme_font_size_override("font_size", 21)
+	
