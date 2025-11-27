917c4450f4ebc272f5098368e2bc45a380c2f842
Yuushi!! <yoshiyushi77@gmail.com>
2025-11-27 23:28:30 +0700

    attempted merge from main
    nothing wrong with it from my testing

--- scripts/clock.gd
+++ scripts/clock.gd
@@ -5,6 +5,6 @@ var rng = RandomNumberGenerator.new()
 
 func advance_time(amount: int = 1):
 	frame += amount
-	tick.pitch_scale = 1
+	tick.pitch_scale = 1.0
 	tick.pitch_scale += rng.randf_range(-0.05, 0.05)
 	tick.play()
