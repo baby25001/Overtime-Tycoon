0e3219d26910eafe406000e137519fb89e9cb379
Yuushi!! <yoshiyushi77@gmail.com>
2025-11-28 01:31:47 +0700

    all audio stuff reimplemented
    meeting is broken for some reason?

--- dailogue test/dialogue.gd
+++ dailogue test/dialogue.gd
@@ -101,7 +101,8 @@ func finish(fail: bool = false):
 	
 	count = 0
 	if was_printing == DIALOGUE:
-		var reward = 0 if fail else GM.add_money_telephone
+		var reward = GM.add_money_telephone
+		if fail: reward = 0
 		affect_sanityBar.emit(false, self)
 		sanity_timer.stop()
 		call_dialogue_finished.emit(reward)
@@ -147,6 +148,12 @@ func _on_meeting_dialogue() -> void:
 		finish(true)
 	
 	prev_meet = await dialogue_from_dict(MEETING, meeting_library, prev_meet)
+
+func force_fail():
+	if printing != DIALOGUE:
+		return
+	
+	finish(true)
 
 func dialogue_from_dict(type: Variant, dict: Dictionary, previous: int = -1) -> int:
 	if count < dialogue.size():
